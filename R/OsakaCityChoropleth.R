dae704fd16af09900e49487fb482c1d2d9dce3e8
Hiroaki Yutani <yutani.ini@gmail.com>
2015-05-31 22:45:53 +0900

    add examples

--- R/OsakaCityChoropleth.R
+++ R/OsakaCityChoropleth.R
@@ -3,6 +3,22 @@
 #' @name osakaCityChoropleth
 #' @importFrom ggplot2 geom_text aes
 #' @importFrom R6 R6Class
+#' @examples
+#' \dontrun{
+#' library(ggplot2)
+#' library(dplyr)
+#'
+#' csv_file <- tempfile(fileext = ".csv")
+#' download.file("https://raw.githubusercontent.com/yutannihilation/osaka_age_composition/master/osaka_age_composition.csv",
+#'            destfile = csv_file, method = "curl")
+#'
+#' age_comp.df <- read.csv(csv_file, header = TRUE, stringsAsFactors = FALSE, fileEncoding = "UTF-8") %>%
+#'   group_by(district) %>%
+#'   summarise(value = sum(age * total)/sum(total)) %>%
+#'   mutate(region = to_osaka_ward_code(district))
+#'
+#' osaka_city_chropleth(age_comp.df, title = "Age Composition") + coord_equal()
+#' }
 #' @export
 osakaCityChoropleth <- R6Class(
   "osakaCityChoropleth",
--- R/util.R
+++ R/util.R
@@ -1,7 +1,20 @@
-#' Convert ward code from/to ward name
+#' Convert ward codes from/to ward names
 #'
 #' @name osaka_city.code
 #' @importFrom stringr str_replace_all
+#'
+#' @examples
+#' # no problem whether the type of the given argument is integer or character
+#' to_osaka_ward_name(c(27127, 27104, 27118))
+#' to_osaka_ward_name(c("27127", "27104", "27118"))
+#'
+#' # get alphabet name
+#' to_osaka_ward_name(c(27127, 27104, 27118), to = "alphabet")
+#'
+#' # get code from ward name (case-insensitive)
+#' # '-ku'/'区' suffix is automatically appended
+#' to_osaka_ward_code(c("北区", "此花", "城東"))
+#' to_osaka_ward_code(c("Kita", "konohana-ku", "JOTO"), from = "alphabet")
 #' @export
 to_osaka_ward_name <- function(ward_code, to = c("kanji", "alphabet")) {
   to <- match.arg(to)
@@ -25,10 +38,10 @@ to_osaka_ward_code <- function(ward_name, from = c("kanji", "alphabet")) {
   } else if(from == "alphabet") {
     # convert to plain ASCII characters
     # add -ku suffix if no -ku
-    ward_name_ <- iconv(ward_name, "UTF-8", "ASCII//TRANSLIT")
+    ward_name_ <- tolower(iconv(ward_name, "UTF-8", "ASCII//TRANSLIT"))
     ward_name_ <- str_replace_all(ward_name_, "([^(?!\\-ku)])$", "\\1-ku")
 
-    matchee    <- iconv(osaka_city.code[, from], "UTF-8", "ASCII//TRANSLIT")
+    matchee    <- tolower(iconv(osaka_city.code[, from], "UTF-8", "ASCII//TRANSLIT"))
   } else {
     stop('`from` must be "kanji" or "alphabet"')
   }
